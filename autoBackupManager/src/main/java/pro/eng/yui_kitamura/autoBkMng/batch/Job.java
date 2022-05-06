767dcbc236465db5c018bfbc575e4913dcc430af
yuiKitamura <yuiKitamura@DESKTOP-T7H9TGC.pro.eng.yui-kitamura.domain>
2022-05-06 16:17:19 +0900

    implement find dir

--- autoBackupManager/src/main/java/pro/eng/yui_kitamura/autoBkMng/batch/Job.java
+++ autoBackupManager/src/main/java/pro/eng/yui_kitamura/autoBkMng/batch/Job.java
@@ -2,19 +2,44 @@ package pro.eng.yui_kitamura.autoBkMng.batch;
 
 import static pro.eng.yui_kitamura.autoBkMng.AutoBackupManager.*;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
 import java.lang.System.Logger.Level;
+import java.nio.file.Path;
 
 public class Job{
+	
+	private Path bkFilPath;
 	
 	public void run() {
 		
 		logger.log(Level.INFO, "batch Job START running -->");
 		
-		
+		findPath();
+	
 		
 		logger.log(Level.INFO, "batch Job END running -->");
 		
 	}
 	
+	private void findPath() {
+		
+		String bkPathProp = getProp("backupDir");
+		
+		File dir = new File(bkPathProp);
+		if(!dir.exists()) {
+			String warnMsg = "prop backupDir does not exists";
+			logger.log(Level.WARNING, warnMsg);
+			throw new RuntimeException(new FileNotFoundException(warnMsg));
+		}
+		if(!dir.isDirectory()) {
+			String warnMsg = "prop backupDir is not a directory";
+			logger.log(Level.WARNING, warnMsg);
+			throw new RuntimeException(new IOException(warnMsg));
+		}
+		
+		bkFilPath = dir.toPath();
+	}
 	
 }
